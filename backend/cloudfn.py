a7f98aad50468ec3c88ea93f2e4b14b8d56be077
Nicholas Polimeni <nicholas.polimeni@outlook.com>
2023-11-19 04:09:54 -0500

    finish prompt engineering and st

--- backend/cloudfn.py
+++ backend/cloudfn.py
@@ -7,11 +7,11 @@ from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-DB_INSTANCE = os.environ(INSTANCE)
-DB_USER = os.environ(USER)
-DB_PASS = os.environ(PASS)
-DB_NAME = os.environ(NAME)
-CLAUDE_KEY = os.environ(CLAUDE_KEY)
+DB_INSTANCE = os.environ.get("INSTANCE")
+DB_USER = os.environ.get("USER")
+DB_PASS = os.environ.get("PASS")
+DB_NAME = os.environ.get("NAME")
+CLAUDE_KEY = os.environ.get("CLAUDE_KEY")
 K_NEAREST = 10
 
 
@@ -46,25 +46,42 @@ def fetch_k_nearest(embedded_query, k):
     pool = getpool()
     with pool.connect() as db_conn:
         result = db_conn.execute(
-            select, parameters={"embedding": embedded_query, "limit": k}
+            select, parameters={"embedding": str(list(embedded_query)), "limit": k}
         ).fetchall()
 
-    return [str({str(row[0]): str(row[1])}) for row in result]
+    return [str({str(row[0]).replace(".txt", ""): str(row[1])}) for row in result]
 
 
 def prompt(query, context):
     input = f"""
-        \n\nHuman: Here is my question: {query};
-        Here is some context and data for you to
-        answer my question with- when answering,
-        please be sure to reference this data exactly.
-        Always include the file name (txt) key. Context: 
-        {context}\n\nAssistant:
+        \n\nHuman: QUESTION: Here is a question about United States (US) legislation: {query};
+        DIRECTIONS: Soon I will provide some context and data for you to answer this question. 
+        This context is a dictionary with legislative bill names as keys and extracted excerpts as
+        values. These bills have been proposed in either the US House or Senate. Senate bills have an S in their file name while
+        House bills have HR in their file name. When answering,
+        please be sure to reference this data in your answer. Please format your
+        answer as follows:
+            {{your general answer based on the given context and your existing knowledge}}
+            {{specific bill #1:\n
+            a direct quote from the bill\n
+            -bullet 1 that explains how this bill is related to the query
+            -bullet 2...}}.
+        Do this for as many bills as you deem fit. Please remain consistent with the above format
+        to ensure the answer is readable with bullet points, which can be denoted by a dash (-).
+        Always include the bill name. Please be detailed regarding how this legislation relates
+        to my question, citing parts of the context as often as possible.
+        Feel free to use any additional knowledge if it will improve the answer but the
+        given context is the most important. Please only provide the answer, do not directly
+        acknowledge my formatting requests in your answer. For instance, do not say that you
+        have summarized documents I provided.\n
+        CONTEXT: {context}\n\nAssistant:
         """
 
     anthropic = Anthropic(api_key=CLAUDE_KEY)
-    return anthropic.completions.create(
-        model="claude-2", max_tokens_to_sample=5000, prompt=input
+    return str(
+        anthropic.completions.create(
+            model="claude-2", max_tokens_to_sample=5000, prompt=input
+        ).completion
     )
 
 
@@ -90,4 +107,4 @@ def answer_query(request):
     embedded_query = embed_query(query)
     context = fetch_k_nearest(embedded_query, K_NEAREST)
 
-    return prompt(query, context)
+    return {"answer": prompt(query, str(context)), "context": context}
--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,61 @@
 # app.py
 import streamlit as st
+import requests
+import json
+import ast
+import re
+
+
+def query_llm(query):
+    url = "https://us-east4-legistlation-llm.cloudfunctions.net/llm-backend"
+    data = {"query": query}
+    response = requests.post(
+        url, data=json.dumps(data), headers={"Content-Type": "application/json"}
+    ).json()
+    return response["answer"], response["context"]
+
 
 def main():
-    st.title("Simple Streamlit App with Text Input")
+    st.title("Legislation LLM Scanner / Research Assistant")
+    st.write(
+        """
+        The project aims to use NLP to build a ChatGPT-like chatbot, 
+        where users can query about recent legislation and get a 
+        summary of the bill. The chatbot would also be able to answer 
+        questions about the bill, such as "What is the purpose of this bill?" 
+        or "What is the impact of this bill?"
+        
+        Please find more information [here](https://devpost.com/software/legislation-llm).
+        
+        **Authors: [Nicholas Polimeni](https://www.linkedin.com/in/nickpolimeni/), 
+        [Faris Durrani](https://www.linkedin.com/in/farisdurrani/), [Justin Singh](https://www.linkedin.com/in/justin-singh-/)**
+        """
+    )
+    st.divider()
+    st.subheader("Enter a query about US legislation")
+    user_input = st.text_input("Input", label_visibility="hidden")
 
-    # Add a text input widget
-    user_input = st.text_input("Enter some text:")
+    if user_input:
+        with st.spinner(
+            "Please wait while we analyze thousands of documents (avg response time: 10 sec)..."
+        ):
+            answer, context = query_llm(user_input)
+            context = [ast.literal_eval(item) for item in context]
+            st.divider()
+            st.subheader("Answer")
+            st.write(answer)
+            st.write("")
+            st.subheader("Related Bills and Sections")
+            for bill in context:
+                for key, val in bill.items():
+                    key = key.replace(".txt", "").upper()
 
-    # Display the entered text
-    st.write("You entered:", user_input)
+                    exp = r"[^\w\n .()]+"
+                    val = re.sub(exp, "", val)
+
+                    with st.expander(f"BILL {key}"):
+                        st.text(val)
+
 
 if __name__ == "__main__":
     main()
